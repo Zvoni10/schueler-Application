85d6593b22f7e547bd67786d6df0f06214bae598
Zvoni10 <Marino@Palijan.de>
2026-09-21 19:11:49 +0200

    Bug Patch

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,1 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  root: ".",
-  build: {
-    outDir: "../server/public",
-    emptyOutDir: true,
-  },
-  server: { port: 5173 },
-});
+import { defineConfig } from "vite"; import react from "@vitejs/plugin-react"; export default defineConfig({ plugins: [react()], base: "./", root: ".", build: { outDir: "../server/public", emptyOutDir: true, }, server: { port: 5173 }, });
